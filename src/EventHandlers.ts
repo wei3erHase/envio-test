f1a6e620ecf00971f9b8a6f7d0eb31c5d41ecaa7
Weißer Hase <wei3erHase@protonmail.com>
2023-09-07 15:14:36 +0200

    chore: initial commit

---
+++ src/EventHandlers.ts
@@ -0,0 +1,18 @@
+import {
+    SAFEEngineContract_ModifySAFECollateralization_handler,
+} from "../generated/src/Handlers.gen";
+
+SAFEEngineContract_ModifySAFECollateralization_handler(({event, context}) => {
+    console.log(event);
+    console.log(context);
+
+    context.safeModification.set({
+        id: event.blockHash.toString(),
+        cType: event.params._cType.toString(),
+        safe: event.params._safe.toString(),
+        collateralSource: event.params._collateralSource.toString(),
+        debtDestination: event.params._debtDestination.toString(),
+        deltaCollateral: BigInt(event.params._deltaCollateral.toString()),
+        deltaDebt: BigInt(event.params._deltaDebt.toString()),
+      });
+});
